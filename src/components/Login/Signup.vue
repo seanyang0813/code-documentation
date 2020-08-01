d46499ce2fa1e30f34a5df1673750947fae46dc6
Sean Yang <seanyang0813@gmail.com>
2020-08-01 17:02:14 -0400

    login and singup process

--- src/components/Login/Signup.vue
+++ src/components/Login/Signup.vue
@@ -2,7 +2,7 @@
   <div class="flex justify-center">
     <div class="w-full max-w-xs  pt-10">
       <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <h1 class="font-bold text-3xl mb-3">Sign in</h1>
+        <h1 class="font-bold text-3xl mb-3">Register</h1>
         <div class="mb-4">
           <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
             Email
@@ -14,9 +14,13 @@
             Password
           </label>          
           <input v-model="password" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
-          <p v-if="error" class="text-red-500 text-xs italic">Invalid email or password</p>
         </div>
-        <p class="text-xs mb-5">Don't have an account? <router-link to='/Signup' class="text-blue-600 hover:text-blue-800 text-xs" href="#">Sign up</router-link></p>  
+        <div class="mb-6">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="password2">
+            Confirm Password
+          </label>          
+          <input v-model="password2" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password2" type="password" placeholder="******************">
+        </div>
         <div class="flex items-center justify-between">
           <button @click="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
             Sign In
@@ -36,40 +40,23 @@ export default {
     return {  
       email: "",
       password: "",  
-      error: false
+      password2: "",
     };
-  },
-  watch: {
-    email() {
-      if (this.email != '') {
-        this.error = false;
-      }
-    },
-    password() {
-      if (this.password != '') {
-        this.error = false;
-      }
-    }
   },
   methods: {
     submit() {
-      firebase.auth().signInWithEmailAndPassword(this.email, this.password)
-      .then((result)=> {
-        console.log(result)
-      }).catch((error) =>{
-        // Handle error.
-        this.email = "";
-        this.password = "";
-        this.error = true;
+      firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+        .catch((error) => {
+        // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
-        console.log(errorCode);
-        console.log(errorMessage);
-      });
-      var user = firebase.auth().currentUser;
-      if (user) {
-        console.log(user.uid);
-      }
+        if (errorCode == 'auth/weak-password') {
+            alert('The password is too weak.');
+        } else {
+            alert(errorMessage);
+        }
+        console.log(error);
+        });
     }
   }
 };
--- src/route.js
+++ src/route.js
@@ -1,9 +1,11 @@
 import Library from './components/library/Library.vue';
 import Editor from './components/editor/Editor.vue';
 import Login from './components/Login/Login.vue';
+import Signup from './components/Login/Signup.vue';
 export const routes = [
     {path: '/', component: Editor},
     {path: '/library', component: Library},
     {path: '/editor', component: Editor},
     {path: '/login', component: Login},
+    {path: '/signup', component: Signup}
 ]
